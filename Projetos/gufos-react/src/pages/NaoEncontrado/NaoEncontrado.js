e35bd9eb2627190343409405aeb7ef4dfe3f3a11
Gabriel Rodrigues Amador <44680781890@info.local>
2019-10-02 17:29:50 -0300

    Proj Viacep Finalized

---
+++ Projetos/gufos-react/src/pages/NaoEncontrado/NaoEncontrado.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../../assets/css/naoEncontrado.css';
+
+class NaoEncontrado extends Component{
+    render(){
+        return(
+            <div className='div'>404 NOT FOUND</div>
+        );
+    }
+}
+
+export default NaoEncontrado;
